7e3c7a6473077c84988656fc6dfd890d88f1e087
nov25emberoff <nov25ember.off@gmail.com>
2026-08-01 16:02:40 +0500

    Create Color.kt

---
+++ app/src/main/java/com/novalink/app/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.novalink.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NovaOrange = Color(0xFFFF7A00)
+val NovaOrangeDark = Color(0xFFCC5F00)
+val NovaOrangeLight = Color(0xFFFF9E40)
+
+val NovaBlack = Color(0xFF000000)
+val NovaSurface = Color(0xFF121212)
+val NovaSurfaceVariant = Color(0xFF1E1E1E)
+val NovaOnSurface = Color(0xFFF2F2F2)
+val NovaOnSurfaceMuted = Color(0xFFAAAAAA)
+val NovaBubbleMine = Color(0xFFFF7A00)
+val NovaBubbleOther = Color(0xFF1E1E1E)
